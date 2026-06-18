26cda1e40d1d1c207f8dafd04d78249fad4e5543
Cesar <billypalacios616@gmail.com>
2026-06-18 10:37:16 -0400

    feat(transaction): finalizando rotas

--- src/routes/transection.router.ts
+++ src/routes/transection.router.ts
@@ -6,5 +6,9 @@ const router = Router();
 const transactionController = new TransectionController();
 
 router.post("/transaction", authMiddleware ,transactionController.create);
+router.get("/transaction", authMiddleware ,transactionController.readAll);
+router.get("/transaction/:id", authMiddleware ,transactionController.readById);
+router.put("/transaction/:id", authMiddleware ,transactionController.update);
+router.delete("/transaction/:id", authMiddleware ,transactionController.delete);
 
 export default router;
